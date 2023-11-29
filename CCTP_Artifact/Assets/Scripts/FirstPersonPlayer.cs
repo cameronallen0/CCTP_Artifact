94001ec2552e96d6178481344d0f20cb3f18624a
cameronallen0 <cameron2.allen@live.uwe.ac.uk>
2023-11-29 17:54:11 +0000

    adding to repo

---
+++ CCTP_Artifact/Assets/Scripts/FirstPersonPlayer.cs
@@ -0,0 +1,181 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class FirstPersonPlayer : MonoBehaviour
+{
+    private PlayerControls inputActions;
+
+    private CharacterController controller;
+    
+    [SerializeField] private Camera cam;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float walkSpeed = 6f;
+    [SerializeField] private float runSpeed = 10f;
+    [SerializeField] public float lookSensitivity = 1.0f;
+    
+    private float xRotation = 0f;
+
+    // Movement Variables
+    private Vector3 velocity;
+    public float gravity = -19.62f;
+    private bool grounded;
+    private bool isRunning;
+
+    //Jump Variables
+    [SerializeField] private float jumpHeight = 3.0f;
+    private bool isJumping;
+
+    // Zoom Variables
+    public float zoomFOV = 35.0f;
+    public float zoomSpeed = 9f;
+    private float targetFOV;
+    private float baseFOV;
+
+    // Crouch Variables
+    private float initHeight;
+    [SerializeField] private float crouchHeight;
+
+    private void Awake()
+    {
+        inputActions = new PlayerControls();
+    }
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        initHeight = controller.height;
+        Cursor.lockState = CursorLockMode.Locked;
+        SetBaseFOV(cam.fieldOfView);
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void Update()
+    {
+        DoMovement();
+        DoLooking();
+        DoZoom();
+        DoCrouch();
+        DoJump();
+        DoRun();
+    }
+
+    private void DoLooking()
+    {
+        Vector2 looking = GetPlayerLook();
+        float lookX = looking.x * lookSensitivity * Time.deltaTime;
+        float lookY = looking.y * lookSensitivity * Time.deltaTime;
+
+        xRotation -= lookY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        
+        transform.Rotate(Vector3.up * lookX);
+    }
+
+    private void DoMovement()
+    {
+        grounded = controller.isGrounded;
+        if (grounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            isJumping = false;
+            isRunning = false;
+        }
+
+        Vector2 movement = GetPlayerMovement();
+        Vector3 move = transform.right * movement.x + transform.forward * movement.y;
+        controller.Move(move * movementSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void DoRun()
+    {
+        if(inputActions.FPSController.Run.ReadValue<float>() > 0)
+        {
+            isRunning = !isRunning;
+            movementSpeed = runSpeed;
+        }
+        else
+        {
+            movementSpeed = walkSpeed;
+        }
+    }
+
+    private void DoZoom()
+    {
+        if (inputActions.FPSController.Zoom.ReadValue<float>() > 0)
+        {
+            targetFOV = zoomFOV;
+        }
+        else
+        {
+            targetFOV = baseFOV;
+        }
+        UpdateZoom();
+    }
+
+    private void DoCrouch()
+    {
+        if (inputActions.FPSController.Crouch.ReadValue<float>() > 0)
+        {
+            controller.height = crouchHeight;
+        }
+        else
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), 2.0f, -1))
+            {
+                controller.height = crouchHeight;
+            }
+            else
+            {
+                controller.height = initHeight;
+            }
+        }
+    }
+
+    private void DoJump()
+    {
+        if(grounded)
+        {
+            if (inputActions.FPSController.Jump.triggered)
+            {
+                isJumping = !isJumping;
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+        }
+    }
+
+    private void UpdateZoom()
+    {
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetFOV, zoomSpeed * Time.deltaTime);
+    }
+
+    public void SetBaseFOV(float fov)
+    {
+        baseFOV = fov;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    public Vector2 GetPlayerMovement()
+    {
+        return inputActions.FPSController.Move.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetPlayerLook()
+    {
+        return inputActions.FPSController.Look.ReadValue<Vector2>();
+    }
+}
